39dba334d2101658c67c6672e2627af754e13ae7
NathyG2524 <nathygirma2524@gmail.com>
2022-06-17 01:46:50 +0300

    Pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/extensions */
 // eslint-disable-next-line no-unused-vars
-import Currency from './3-currency.js'; 
+import Currency from './3-currency.js';
 
 export default class Pricing {
   constructor(amount, currency) {
