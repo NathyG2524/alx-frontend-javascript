8b9bf2f542b3fa303f8c42edaf679104fcb52e3a
NathyG2524 <nathygirma2524@gmail.com>
2022-06-17 01:59:32 +0300

    EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,6 +1,6 @@
 /* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
 /* eslint class-methods-use-this: ["error", { "exceptMethods": ["cloneCar"] }] */
-import Car from './10-car';
+import Car from './10-car.js';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
