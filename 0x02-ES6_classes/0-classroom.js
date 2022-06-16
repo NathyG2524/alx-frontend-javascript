a32c5c52e189334aa436b21c430b57841f6965e4
NathyG2524 <nathygirma2524@gmail.com>
2022-06-16 20:23:09 +0300

    You used to attend a place like this at some point

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+// Prototype: export default class ClassRoom
+// It should accept one attribute named maxStudentsSize (Number) and assigned to _maxStudentsSize
+
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
