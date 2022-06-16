4e11ce826a6922e4ec71049d174d78df0211a93f
NathyG2524 <nathygirma2524@gmail.com>
2022-06-16 21:08:40 +0300

    let's make some classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,7 +2,8 @@
 // eslint-disable-next-line import/extensions
 import ClassRoom from './0-classroom.js';
 
-// Implement a function named initializeRooms. It should return an array of 3 ClassRoom objects with the sizes 19, 20, and 34 (in this order).
+// Implement a function named initializeRooms.
+// It should return an array of 3 ClassRoom objects with the sizes 19, 20, and 34 (in this order).
 
 export default function initializeRooms() {
   return [
