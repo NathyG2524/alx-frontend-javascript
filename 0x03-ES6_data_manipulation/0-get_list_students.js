d5c1840509db8f64946e73ff19256acc8189d85f
NathyG2524 <nathygirma2524@gmail.com>
2022-06-20 20:54:29 +0300

    basic list of object

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,17 @@
+export default function getListStudents() {
+  return [{
+    id: 1,
+    firstNmae: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstNmae: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 3,
+    firstNmae: 'Serena',
+    location: 'San Francisco',
+  }];
+}
